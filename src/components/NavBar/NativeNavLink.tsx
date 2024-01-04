fc08cf79d617f958067eaecb6863e1df0c414cdb
Jellyton <jellyton69@gmail.com>
2024-01-04 01:12:07 -0800

    tweak(nativeNavLink): memoize component

---
+++ src/components/NavBar/NativeNavLink.tsx
@@ -0,0 +1,26 @@
+import { Text, NavLink, CSSProperties, rem } from "@mantine/core";
+import { NavLink as RouteLink, Location } from "react-router-dom";
+import { camelCaseFromSnakeCase } from "../../utils/stringUtils";
+import { memo } from "react";
+
+const categoryStyle: CSSProperties = {
+	borderRadius: rem(6),
+	padding: `${rem(6)} ${rem(12)}`,
+	transition: "all 0.3s ease", // Customize as needed
+};
+
+function NativeNavLink({ nativeHash, nativeData, formattedCategoryName, location }: { nativeHash: string; nativeData: any; formattedCategoryName: string; location: Location }) {
+	return (
+		<NavLink
+			h={30}
+			key={nativeHash}
+			style={categoryStyle}
+			component={RouteLink}
+			to={"/docs/natives/" + formattedCategoryName + "/" + nativeHash}
+			active={location.pathname == "/docs/natives/" + formattedCategoryName + "/" + nativeHash}
+			label={<Text fz={13}>{(nativeData.name && camelCaseFromSnakeCase(nativeData?.name)) || nativeHash}</Text>}
+		/>
+	);
+}
+
+export default memo(NativeNavLink);
