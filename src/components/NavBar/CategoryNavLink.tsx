f59719469a1ec9dd23ed50e7e539910b12e3c462
Jellyton <jellyton69@gmail.com>
2024-01-04 01:13:02 -0800

    refactor(navbar): virtualize rendering of native list

--- src/components/NavBar/CategoryNavLink.tsx
+++ src/components/NavBar/CategoryNavLink.tsx
@@ -1,2 +1,7 @@
+					<Virtuoso
+						style={{ width: "95%", height: listHeight }}
+						data={filteredNatives}
+						itemContent={(index, [nativeHash, nativeData]) => (
 							<NativeNavLink key={nativeHash} nativeHash={nativeHash} nativeData={nativeData} formattedCategoryName={formattedCategoryName} location={location} />
 						)}
+					/>
