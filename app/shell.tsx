2ec9bd2179e6e21ac2b42aa14d3bef776de53ea9
Jellyton <jellyton69@gmail.com>
2024-05-09 02:59:32 -0700

    Add header to layout in prep for future page additions

--- app/shell.tsx
+++ app/shell.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { Center, Loader, AppShell } from "@mantine/core";
+import { Text, Center, Loader, AppShell, rem, Group } from "@mantine/core";
 import { useEffect } from "react";
 import NavBar from "./_components/NavBar/NavBar";
 import { useNativesStore } from "./_stores/NativesStore";
-import { getNatives } from "./_data/natives";
+import Footer from "./_components/Footer/Footer";
+import { useHeadroom } from "@mantine/hooks";
 import getBaseURL from "./_utils/getBaseURL";
 
 const baseURL = getBaseURL();
 
 export default function Shell({ children }: { children: React.ReactNode }) {
 	const { setNatives, getAllCategories } = useNativesStore();
+	const pinned = useHeadroom({ fixedAt: 60 });
 
 	useEffect(() => {
 		if (getAllCategories()?.length > 0) return;
@@ -18,10 +20,7 @@ export default function Shell({ children }: { children: React.ReactNode }) {
 			setNatives(await fetch(baseURL + "/api/natives").then((res) => res.json()));
 		};
 
-		// call the function
-		fetchData()
-			// make sure to catch any error
-			.catch(console.error);
+		fetchData().catch(console.error);
 	}, []);
 
 	if (getAllCategories()?.length <= 0)
@@ -32,11 +31,20 @@ export default function Shell({ children }: { children: React.ReactNode }) {
 		);
 
 	return (
-		<AppShell h="100vh" navbar={{ width: "22rem", breakpoint: "sm" }}>
+		<AppShell navbar={{ width: 350, breakpoint: "sm" }} header={{ height: 60, collapsed: !pinned, offset: true }}>
+			<AppShell.Header>
+				<Group h="100%" align="center" px={20} py={6}>
+					<Text fz={30} fw={700} c="white">
+						Native Documentation
+					</Text>
+				</Group>
+			</AppShell.Header>
+
 			<AppShell.Navbar>
 				<NavBar />
 			</AppShell.Navbar>
-			<AppShell.Main h="100%">{children}</AppShell.Main>
+
+			<AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`} /*pb={`calc(${rem(220)} + var(--mantine-spacing-md))`}*/>{children}</AppShell.Main>
 		</AppShell>
 	);
 }
