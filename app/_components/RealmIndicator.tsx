a0b312b8ff3e3304095324631c282090ea461c40
Jellyton <jellyton69@gmail.com>
2024-08-31 03:46:07 -0700

    implement tailwind and shadcn

--- app/_components/RealmIndicator.tsx
+++ app/_components/RealmIndicator.tsx
@@ -1,11 +1,11 @@
-import { Box, Tooltip, useMantineTheme } from "@mantine/core";
 import { capitalizeFirstLetter } from "../_utils/stringUtils";
+import { cn } from "@/lib/utils";
 
 type Realm = "client" | "server" | "shared";
 type RealmKeys = "client" | "server";
 const realmMapper: Record<RealmKeys, string> = {
-	client: "orange",
-	server: "blue",
+  client: "bg-orange-600",
+  server: "bg-blue-600",
 };
 
 const gradient = `linear-gradient(
@@ -14,23 +14,33 @@ const gradient = `linear-gradient(
 	#F76707 50%
 )`;
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
 function RealmIndicator(props: { realm: Realm }) {
-	const { realm } = props;
+  const { realm } = props;
 
-	const theme = useMantineTheme();
-
-	return (
-		<Tooltip label={capitalizeFirstLetter(realm)}>
-			<Box
-				bg={(realm == "shared" && gradient) || realmMapper[realm]}
-				w={22}
-				h={22}
-				style={{
-					borderRadius: theme.radius.md,
-				}}
-			/>
-		</Tooltip>
-	);
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            className={cn("size-6 rounded-full", realm != "shared" && realmMapper[realm])}
+            style={{
+              background: realm == "shared" && gradient,
+            }}
+          />
+        </TooltipTrigger>
+        <TooltipContent
+          className={cn("text-lg font-bold text-neutral-100", realm != "shared" && realmMapper[realm])}
+          style={{
+            background: realm == "shared" && gradient,
+          }}
+        >
+          {capitalizeFirstLetter(realm)}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
 
 export default RealmIndicator;
