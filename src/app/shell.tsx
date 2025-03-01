8e877426926fad3e7b2d98f5d6977fd57b902c9f
Jellyton <20498875+jellyton255@users.noreply.github.com>
2025-03-01 02:09:52 -0800

    tweak: remove title in header

--- src/app/shell.tsx
+++ src/app/shell.tsx
@@ -6,9 +6,6 @@ export default async function Shell({ children }: { children: React.ReactNode })
   return (
     <div className="flex h-screen w-screen flex-col overflow-x-hidden">
       <div className="flex w-full items-center gap-2 p-2">
-        <h1 className="scroll-m-20 text-3xl font-extrabold leading-none tracking-tight lg:text-4xl">
-          <span className="text-neutral-500">(Unofficial)</span> FiveM Docs
-        </h1>
         <Link href="/" className="px-4 pb-4" passHref legacyBehavior>
           <Button variant="secondary">Home</Button>
         </Link>
